9a03b7b7da3959afe4ee074f1dec884403ce4c06
SAPNA <47707734+sonalisikarwar@users.noreply.github.com>
2020-12-25 19:24:33 +0530

    Add files via upload
    annotations in java

---
+++ AutoBox.java
@@ -0,0 +1,12 @@
+//demo autoboxing /unboxing
+ 
+package shivaafoundation.javatutorial;
+
+
+ class AutoBox {
+ public static void main(String args[]){
+        Integer iOb = 100;
+        int i=iOb;
+        System.out.println(i +  " "  + iOb);
+    }   
+}
